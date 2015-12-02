4332aa8f90b59853e0de804a63381249ed9de54f
rasdasd <dave.hirschberg@gmail.com>
2015-12-02 13:47:21 -0800

    Initial Commit

--- app/src/main/java/rasdasd/com/handstodrums/ML1_Holder.java
+++ app/src/main/java/rasdasd/com/handstodrums/ML1_Holder.java
@@ -37,7 +37,7 @@ public class ML1_Holder implements Holder {
         {
             layers[i] = layers[i-1]-descent;
         }
-        layers[layerCount] = outputsize;
+        layers[layerCount-1] = outputsize;
         HyperbolicTransfer traFunc = new HyperbolicTransfer();
         MultiLayerPerceptron mlp = new MultiLayerPerceptron(layers,0.5,traFunc);
     }
