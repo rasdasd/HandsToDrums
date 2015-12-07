c29844937409dc29c4eec8f24e22345c53bed34e
rasdasd <dave.hirschberg@gmail.com>
2015-12-06 22:54:27 -0800

    Clustering merged properly

--- app/src/main/java/rasdasd/com/handstodrums/ML3_Holder.java
+++ app/src/main/java/rasdasd/com/handstodrums/ML3_Holder.java
@@ -1,68 +1,84 @@
 package rasdasd.com.handstodrums;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 
 /**
  * Created by David on 10/21/2015.
  */
-public class ML3_Holder implements Holder {
+//Updated Jeremy Li 28 on 12/6/2015
+public class ML3_Holder implements Holder{
     ArrayList<ArrayList<double[]>> data = new ArrayList<ArrayList<double[]>>();
-    ArrayList<double[]> desiredouts = new ArrayList<double[]>();
-    public int classes = 0;
-    MLP mlp;
-    int outputsize;
-    int iterations = 10;
+    private ArrayList<double[]> centers = new ArrayList<double[]>();
+    private int inputsize;
+    public int classes;
+    private static final String TAG = "MyActivity";
 
-    public ML3_Holder(int classes, int inputsize) {
-        outputsize = Integer.SIZE - Integer.numberOfLeadingZeros(classes - 1);
+    public ML3_Holder(int classes, int inputsize)
+    {
         this.classes = classes;
-        for (int i = 0; i < classes; i++) {
+        this.inputsize = inputsize;
+        for(int i = 0; i < classes; i++)
             data.add(new ArrayList<double[]>());
-            double[] desire = new double[outputsize];
-            for (int j = outputsize - 1; j >= 0; j--) {
-                boolean flag = (i & (1 << j)) != 0;
-                if (flag)
-                    desire[j] = 1;
-                else
-                    desire[j] = 0;
-            }
-            desiredouts.add(desire);
-        }
-        int hidden = (inputsize + outputsize) * 2 / 3;
-        mlp = new MLP(inputsize, hidden, outputsize);
     }
-
-    public void addDataPoint(float[] datapoint, int cata) {
+    public void addDataPoint(float[] datapoint, int cata)
+    {
         double[] output = new double[datapoint.length];
-        for (int i = 0; i < datapoint.length; i++) {
+        for (int i = 0; i < datapoint.length; i++)
+        {
             output[i] = datapoint[i];
         }
         data.get(cata).add(output);
     }
+    public void train()
+    {
 
-    public void train() {
-        for (int i = 0; i < iterations; i++) {
-            for (int c = 0; c < classes; c++) {
-                for (int v = 0; v < data.get(c).size(); v++) {
-                    double[] pattern = data.get(c).get(v);
-                    mlp.train(pattern, desiredouts.get(c));
+        this.centers.clear();
+        for(int i=0; i<data.size();i++){
+            double[] newCenter = new double[inputsize];
+            for(int c=0; c<newCenter.length;c++)
+                newCenter[c] = 0;
+            int counter = 0;
+            for(int k=0; k<data.get(i).size();k++){
+                for(int m=0; m<data.get(i).get(k).length; m++){
+                    newCenter[m] += data.get(i).get(k)[m];
+                    counter ++;
                 }
             }
+            for(int n=0; n<newCenter.length;n++){
+                newCenter[n] /= counter;
+            }
+            this.centers.add(newCenter);
         }
+//        Log.d(TAG, "train: "+this.centers.size());
     }
-
-    public int classify(float[] datapoint) {
-        int cata = -1;
+    public int classify(float[] datapoint)
+    {
+        int cata = 0;
         double[] input = new double[datapoint.length];
-        for (int i = 0; i < datapoint.length; i++) {
+        for (int i = 0; i < datapoint.length; i++)
+        {
             input[i] = datapoint[i];
         }
-        double[] output = mlp.passNet(input);
-        int result = 0;
-        for (int i = output.length - 1; i >= 0; i--)
-            if (output[i] >= 0.5)
-                result += Math.pow(2, (output.length - i - 1));
-        cata = result;
+
+        double max = calculateDist(input, this.centers.get(0));
+        for(int i=0; i< this.centers.size(); i++){
+            double dist = calculateDist(input,this.centers.get(i));
+            if(dist < max){
+                cata = i;
+                max = dist;
+            }
+        }
+//        Log.d(TAG, "classify: "+cata);
         return cata;
+    }
+
+    public double calculateDist(double[] pattern, double[] center){
+        double dist=0;
+        for(int i=0;i<pattern.length;i++){
+            dist += Math.pow(pattern[i]-center[i],2);
+        }
+        return dist;
     }
 }
